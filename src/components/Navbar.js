4e2f70f7daaf934c86197d665b9a2939964c33d3
AB <anitabanhtx@gmail.com>
2023-04-05 15:23:15 -0500

    add email address

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -119,8 +119,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#7F6A93] ">
             <a
               className="flex justify-between items-center w-full text-gray-100"
-              href="/"
-              target="_blank"
+              href='mailto:anitabanhtx@gmail.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
